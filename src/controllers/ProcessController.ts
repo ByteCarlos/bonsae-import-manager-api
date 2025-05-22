e7f025afe36accf5c53897fc81397da166429181
Yuri-IQI <anthony.yuri.ff@gmail.com>
2025-05-22 18:57:10 -0300

    feat: add get methods for retrieving partial and full process data

--- src/controllers/ProcessController.ts
+++ src/controllers/ProcessController.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from "express";
 import ProcessDocument from "../models/documents/ProcessDocument";
+import SchoolPeriodDocument from "../models/documents/SchoolPeriodDocument";
+import SubjectDocument from "../models/documents/SubjectDocument";
+import ClassDocument from "../models/documents/ClassDocument";
+import UserDocument from "../models/documents/UserDocument";
+import ProfessorEnrollmentDocument from "../models/documents/ProfessorEnrollmentDocument";
+import StudentEnrollmentDocument from "../models/documents/StudentEnrollmentDocument";
+import mongoose from "mongoose";
+
+const allowedModels = ['Subject', 'Class', 'User', 'Professor_Enrollment', 'Student_Enrollment', 'School_Period'];
 
 export default {
   async createProcess(req: Request, res: Response) {
@@ -43,7 +52,72 @@ export default {
   },
 
   async getFullProcess(req: Request, res: Response) {
-    // TODO
+    try {
+      const { processId } = req.params;
+
+      if (!processId) {
+        return res.status(400).json({ error: "Missing processId in URL parameters" });
+      }
+
+      const process = await ProcessDocument.findOne({ processId });
+      if (!process) {
+        return res.status(404).json({ error: "Process not found" });
+      }
+
+      const [
+        schoolPeriods,
+        subjects,
+        classes,
+        users,
+        professorEnrollments,
+        studentEnrollments
+      ] = await Promise.all([
+        SchoolPeriodDocument.find({ processId }),
+        SubjectDocument.find({ processId }),
+        ClassDocument.find({ processId }),
+        UserDocument.find({ processId }),
+        ProfessorEnrollmentDocument.find({ processId }),
+        StudentEnrollmentDocument.find({ processId })
+      ]);
+
+      const data = {
+        process,
+        schoolPeriods,
+        subjects,
+        classes,
+        users,
+        professorEnrollments,
+        studentEnrollments
+      };
+
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.status(500).json({ error: (error as Error).message });
+    }
+  },
+
+  async getPartialProcess(req: Request, res: Response) {
+    try {
+      const { processId, model } = req.params;
+
+      if (!allowedModels.includes(model)) {
+        return res.status(400).json({ error: `Model '${model}' is not allowed.` });
+      }
+
+      if (!processId || !model) {
+        return res.status(400).json({ error: "Missing required URL parameters" });
+      }
+
+      const Model = mongoose.model(model);
+      const stepData = await Model.find({ processId });
+      if (!stepData) {
+        return res.status(404).json({ error: `No data found for the model: ${model}` })
+      }
+
+      return res.status(200).json(stepData);
+    } catch (error) {
+      return res.status(500).json({ error: (error as Error).message });
+    }
   },
 
   async getAllProcesses(_req: Request, res: Response) {
@@ -90,5 +164,25 @@ export default {
     } catch (error) {
       return res.status(500).json({ error: (error as Error).message });
     }
-  }
+  },
+
+  async destroyPartial(req: Request, res: Response) {
+    try {
+      const { processId, model } = req.params;
+
+      if (!processId || !model) {
+        return res.status(400).json({ error: "Missing required URL parameters" });
+      }
+      if (!allowedModels.includes(model)) {
+        return res.status(400).json({ error: `Model '${model}' is not allowed.` });
+      }
+
+      const Model = mongoose.model(model);
+      const result = await Model.deleteMany({ processId });
+
+      return res.status(200).json({ message: `${model} data successfully deleted from process ${processId}`, result: result });
+    } catch (error) {
+      return res.status(500).json({ error: (error as Error).message });
+    }
+  },
 };
--- src/routes/process.routes.ts
+++ src/routes/process.routes.ts
@@ -7,7 +7,10 @@ const router = Router();
 router.post('/', asyncHandler(ProcessController.createProcess))
 router.get('/', asyncHandler(ProcessController.getAllProcesses));
 router.get('/:processId', asyncHandler(ProcessController.getProcess))
+router.get('/partial/:processId/:model', asyncHandler(ProcessController.getPartialProcess))
+router.get('/full/:processId', asyncHandler(ProcessController.getFullProcess))
 router.put('/:processId', asyncHandler(ProcessController.update));
 router.delete('/:processId', asyncHandler(ProcessController.destroy));
+router.delete('/partial/:processId/:model', asyncHandler(ProcessController.destroyPartial))
 
 export default router;
