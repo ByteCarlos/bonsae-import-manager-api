935b9fa182b7b13a6dd6e4abf3f7f21aaf9d5921
bytecarlos <109424074+ByteCarlos@users.noreply.github.com>
2025-05-25 16:04:51 -0300

    hotfix: swagger fix assets

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -21,6 +21,12 @@ routes.use('/subject', SubjectRoutes);
 routes.use('/user', UserRoutes);
 routes.use('/import', ImportRoutes);
 routes.use('/process', ProcessRoutes);
-routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js'
+  ]
+}));
 
 export default routes;
